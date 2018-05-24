db6ef3f266ba8d9dcfd805d2f0a86e07f508fe76
oopfan <oopfan@optonline.net>
2018-05-24 17:00:34 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Traversy-DragnDrop
+"Drag & Drop With Vanilla JS" by Traversy Media
+YouTube video [here](https://www.youtube.com/watch?v=C22hQKE_32c)
+
+Play around with it [here](https://oopfan.github.io/Traversy-DragnDrop/public/)
---
+++ public/resources/js/main.js
@@ -0,0 +1,43 @@
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+// Fill Listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Loop through empties and call drag events
+for (const empty of empties) {
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+}
+
+// Drag Functions
+function dragStart(e) {
+  e.dataTransfer.setData("text/plain", e.target.id);
+  this.className += ' hold';
+  setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+  this.className = 'fill';
+}
+
+function dragOver(e) {
+  e.preventDefault();   // Enables dragDrop() to fire
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += ' hovered';
+}
+
+function dragLeave() {
+  this.className = 'empty';
+}
+
+function dragDrop() {
+  this.className = 'empty';
+  this.append(fill);
+}
